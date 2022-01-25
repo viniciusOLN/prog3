8b2da29db1a5fbb3e1e7af940ad52778e7da01d3
Vinicius Oliveira <vinicius853o@gmail.com>
2022-01-25 20:27:53 -0300

    update

--- jogo_advinhacao/jogo_advinhacao/views.py
+++ jogo_advinhacao/jogo_advinhacao/views.py
@@ -2,21 +2,19 @@ from django.shortcuts import render
 from jogo_advinhacao.forms import FormGame
 import random
 
-def newNum():   
-    return random.sample([i for i in range(100)], 1)
+def newNum():
+    lista = [i for i in range(100)]
+    return random.sample(lista, 1)
 
-CHANCES = 5
+def game(data):
+    pass
+
+CHANCES = 0
 NUM = newNum()
 
 def index(request):
     form = FormGame(request.GET or None)
     if form.is_valid():
-        num = NUM        
-        if form['play'] > num:
-            text = 'maior que ' + form['play']            
-        elif form['play'] < num:
-            text = 'menor que ' + form['play']
-        else:
-            text='Parabéns! Você acertou!'
+        num = NUM
 
     return render(request, 'index.html', locals())
