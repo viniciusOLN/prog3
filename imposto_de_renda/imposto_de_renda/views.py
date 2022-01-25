401d73424b0f734786255b6e79e1d3e84908c3ab
Vinicius Oliveira <vinicius853o@gmail.com>
2022-01-25 17:07:30 -0300

    adicionando estilo e lógica

--- imposto_de_renda/imposto_de_renda/views.py
+++ imposto_de_renda/imposto_de_renda/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from imposto_de_renda.forms import FormRenda
 
+def calc(data):
+    dependent = data['dependents'] * 2
+    ir = data['annual_income'] - (data['annual_income'] * dependent/100)    
+
+    if ir >= 0 and ir < 2000:
+        imp = '0,00'
+    elif ir >= 2000 and ir < 5000:
+        imp = data['annual_income'] * 5/100
+    elif ir >= 5000 and ir < 10000:
+        imp = data['annual_income'] * 10/100
+    elif ir >= 10000:
+        imp = data['annual_income'] * 15/100
+    return (ir, imp)
+        
 def index(request):
     form = FormRenda(request.GET or None)
+    if form.is_valid():
+        imp = calc(form.cleaned_data)
+        template_name = 'result.html'
+    else:
+        template_name = 'index.html'
 
-    return render(request, 'index.html', locals())
+    return render(request, template_name, locals())
