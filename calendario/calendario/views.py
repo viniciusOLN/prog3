2505681c42502637c1138c01b64a2731a6f39b50
Vinicius Oliveira <vinicius853o@gmail.com>
2022-01-26 15:39:11 -0300

    terceira questão concluída

--- calendario/calendario/views.py
+++ calendario/calendario/views.py
@@ -10,16 +10,31 @@ def index(request):
         form = FormCalendario(request.POST)
         ano = int(form['year'].value())
         mes = int(form['month'].value())
+        ano_extenso = {
+            "1":"Janeiro",
+            "2":"Fevereiro",
+            "3":"Março",
+            "4":"Abril",
+            "5":"Maio",
+            "6":"Junho",
+            "7":"Julho",
+            "8":"Agosto",
+            "9":"Setembro",
+            "10":"Outubro",
+            "11":"Novembro",
+            "12":"Dezembro",
+        }
 
         if form.is_valid():
             calendar.setfirstweekday(calendar.SUNDAY)
             value = calendar.monthcalendar(ano, mes)
             context = {
                 'form': form,
-                'value': value
+                'value': value,
+                'ano_extenso': ano_extenso[str(mes)][0:3],                
             }
         
-            return render(request, 'index.html', context=context)
+            return render(request, 'calendar.html', context=context)
 
     else:
         form = FormCalendario()
